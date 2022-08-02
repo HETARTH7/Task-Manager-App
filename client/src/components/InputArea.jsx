22c661017e0838e4c875838bd3718cc29c793627
unknown <hetarth002@gmail.com>
2022-08-02 20:27:03 +0530

    implementing axios to add to server from frontend

--- client/src/components/InputArea.jsx
+++ client/src/components/InputArea.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 function InputArea(props) {
   const [inputText, setInputText] = useState("");
@@ -7,16 +8,24 @@ function InputArea(props) {
     const newValue = event.target.value;
     setInputText(newValue);
   }
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const task = {
+      inputText: inputText,
+    };
+    console.log(task);
+    axios
+      .post("http://localhost:5000/add", task)
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+    setInputText("");
+    window.location = "/";
+  };
 
   return (
     <div className="form">
       <input onChange={handleChange} type="text" value={inputText} />
-      <button
-        onClick={() => {
-          props.onAdd(inputText);
-          setInputText("");
-        }}
-      >
+      <button onClick={onSubmit}>
         <span>Add</span>
       </button>
     </div>
